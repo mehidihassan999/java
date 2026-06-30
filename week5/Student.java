515b7a22f4e9503c4f8fba142ba75721069454d1
mehidihassan999 <mehidihassan625@gmail.com>
2026-06-30 12:30:11 +0800

    week5done

---
+++ week5/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+    private String studentID;
+    private String name;
+    private double cgpa;
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCGPA(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCGPA() {
+        return cgpa;
+    }
+
+}
