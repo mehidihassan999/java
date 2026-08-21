60e85d3c2eedc2a07b76121b06b24ea3c5688bee
mehidihassan999 <mehidihassan625@gmail.com>
2026-08-21 19:56:42 +0800

    week_2

---
+++ week_2/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    // Attributes
+    String name;
+    int age;
+    double gpa;
+
+    // Constructor
+    public Student(String studentName, int studentAge, double studentGpa) {
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+    }
+
+    // Methods
+    public void displayInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+        System.out.println("GPA  : " + gpa);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying.");
+    }
+
+    public void takeExam() {
+        System.out.println(name + " is taking an exam.");
+    }
+}
